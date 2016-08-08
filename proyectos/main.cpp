bc8c7a1bb450a74d6977d26bc0bcb98359e4207d
GomezzA <angela.gomez@unet.edu.ve>
2016-08-08 08:47:05 -0400

    Add files via upload

---
+++ proyectos/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdio.h>
+#include <conio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int A,B,C;
+	printf("\n Valor de A:");
+	scanf("%d", &A);
+	printf("\n Valor de B:");
+	scanf("%d", &B);
+	C=A+B;
+	printf("\n\n El resultado es %d", C);
+
+	return 0;
+	
+}
